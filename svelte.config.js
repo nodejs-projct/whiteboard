39efb0205485cdb0ac6a86207b50d664cbe1ea76
jpsi-0895 <jaipal.saini.0895@gmail.com>
2025-11-05 12:39:18 +0530

    build setup

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,8 @@
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import adapter from '@sveltejs/adapter-static';
 
-/** @type {import("@sveltejs/vite-plugin-svelte").SvelteConfig} */
 export default {
-  // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
-  // for more information about preprocessors
-  preprocess: vitePreprocess(),
-}
+  kit: {
+    adapter: adapter(),
+    // other config options
+  }
+};
